2e33124c40ce5414231cf6edddf97ff1891de7f6
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-05 10:07:46 -0700

    Add ajax call to return top word and display upon page load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import $ from 'jquery'
 
+
+function getTopWord() {
+  $.ajax({
+  type: 'GET',
+  url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
+  success: function(word) {
+    $(".word-count").html(`<h3>${Object.keys(word.word)[0]}</h3>`)
+  },
+  error: function(response) {
+    debugger;
+    alert(response.responseJSON.error);
+  }
+  })
+}
+
 $(document).ready(() => {
-  // have fun!
-})
+  getTopWord();
+});
