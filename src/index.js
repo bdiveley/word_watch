2a976fed46c5ff9ee78249544e66aa9a0ece6ae3
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-05 11:25:12 -0700

    Add flash container to html file

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ function getTopWord() {
   url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
   success: function(word) {
     var top_word = Object.keys(word.word)[0]
-    $(".word-count").html(`<h3>${top_word}: ${word.word[top_word]}</h3>`)
+    $(".word-count").html(`<h3>${top_word}<br> word count: ${word.word[top_word]}</h3>`)
   },
   error: function(response) {
-    debugger;
     alert(response.responseJSON.error);
   }
   })
@@ -17,21 +16,22 @@ function getTopWord() {
 
 function postWord(input) {
   $.ajax({
-  type: 'POST',
-  url: 'https://wordwatch-api.herokuapp.com/api/v1/words',
-  data: { "word": { "value": input } },
-  success: function(response) {
-  },
-  error: function(response) {
+    type: 'POST',
+    url: 'https://wordwatch-api.herokuapp.com/api/v1/words',
+    data: { "word": { "value": input } },
+    success: function(response) {
+      $('#flash-container').append(`${response.message}`)
+    },
+    error: function(response) {
     alert(response.responseJSON.error);
-  }
+    }
   })
 }
 
 function splitWords() {
     var words = document.getElementById("message").value.trim().split(" ");
     words.forEach(function(word) {
-      postWord(word);
+      setTimeout(postWord(word), 5000);
     })
 }
 
