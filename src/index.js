338ddc563be466dfc5902bd335e17c9b51e9f9c4
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-05 11:04:28 -0700

    Add ability for user to add word or string of words to database

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import $ from 'jquery'
 
-
 function getTopWord() {
   $.ajax({
   type: 'GET',
   url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
   success: function(word) {
-    $(".word-count").html(`<h3>${Object.keys(word.word)[0]}</h3>`)
+    var top_word = Object.keys(word.word)[0]
+    $(".word-count").html(`<h3>${top_word}: ${word.word[top_word]}</h3>`)
   },
   error: function(response) {
     debugger;
@@ -15,6 +15,31 @@ function getTopWord() {
   })
 }
 
+function postWord(input) {
+  $.ajax({
+  type: 'POST',
+  url: 'https://wordwatch-api.herokuapp.com/api/v1/words',
+  data: { "word": { "value": input } },
+  success: function(response) {
+  },
+  error: function(response) {
+    alert(response.responseJSON.error);
+  }
+  })
+}
+
+function splitWords() {
+    var words = document.getElementById("message").value.trim().split(" ");
+    words.forEach(function(word) {
+      postWord(word);
+    })
+}
+
 $(document).ready(() => {
   getTopWord();
+
+$("#breakdown-btn").on('click', function(){
+  splitWords();
+});
+
 });
